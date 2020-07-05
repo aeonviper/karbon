968b50cd69a7c0f92750cc1c0b2535fdca4cb8e6
aeonviper <viperblaster@gmail.com>
2020-07-05 18:14:08 +0700

    Version 3.0

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM node:10-alpine
 WORKDIR /system
 COPY package.json .
 RUN npm install
-EXPOSE 3000
+EXPOSE 10080
+EXPOSE 10443
 CMD [ "npm", "run", "start" ]
 COPY index.js .
--- index.js
+++ index.js
@@ -1,22 +1,32 @@
 const express = require('express')
 const cors = require('cors')
+const fs = require('fs')
+const http = require('http')
+const https = require('https')
+const httpPort = 10080
+const httpsPort = 10443
+// const certificate = fs.readFileSync('fullchain.pem', 'utf8')
+// const key = fs.readFileSync('privatekey.pem', 'utf8')
 const application = express()
-const port = 8900
 
 application.use(cors())
 application.use(express.json())
 application.use(express.urlencoded({ extended: true }))
 
-application.get('/', (request, response, next) => response.send('Karbon version 2.1'))
+application.get('/', (request, response, next) => response.send('Karbon version 3.0'))
 
 application.post('/uppercase', function (request, response, next) {
-	let data = request.body;
-	if (data.text) {
-		data.text = data.text.toUpperCase();
-		response.json(data);
-	} else {
-		response.json();
-	}
+        let data = request.body;
+        if (data.text) {
+                data.text = data.text.toUpperCase();
+                response.json(data);
+        } else {
+                response.json();
+        }
 })
 
-application.listen(port, () => console.log(`Listening at http://localhost:${port}`))
+const httpServer = http.createServer(application)
+// const httpsServer = https.createServer({ key:key, cert:certificate }, application)
+
+httpServer.listen(httpPort, () => { console.log('Listening at port ' + httpPort) })
+// httpsServer.listen(httpsPort, () => { console.log('Listening at port ' + httpsPort) })
